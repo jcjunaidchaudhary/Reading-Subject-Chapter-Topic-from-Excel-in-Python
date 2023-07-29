b87b07b2262854692912b5b8aca57f81a6415460
Juned Chaudhary <87421043+jcjunaidchaudhary@users.noreply.github.com>
2023-07-29 20:17:25 +0530

    chapter code modified

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ chapter=[]
 topic=[]
 #################################
 
-# Replace 'data.xlsx' with the path to your Excel file if it's in a different location
-file_path = 'data.xlsx'
-
 # Load the workbook
 workbook = openpyxl.load_workbook("NEET-PLAN.xlsx")
 
@@ -23,17 +20,29 @@ print(sheet_names)
 # Select the sheet you want to read from (e.g., the first sheet)
 # sheet = workbook.active
 
+
+def gen_num():
+    n=0
+    while True:
+        yield n
+        n+=1
+
+d=gen_num()
+d.__next__()
+
 data=[]
+chapter_id=0
 for sub_id in range(len(sheet_names)):
     count=0
     sheet = workbook[sheet_names[sub_id]]
     for row in sheet.iter_rows(values_only=True):
         if count==0:
-            chapter.extend([{"id":i+1,"chapter":row[i],"subject":sub_id+1} for i in range(len(row))])
+            chapter.extend([{"id":d.__next__(),"chapter":row[i],"subject":sub_id+1} for i in range(len(row))])
+            chapter_id+=len(row)
             count+=1
             continue
         count+=1
-        topic.extend([{"id":i+1,"topic":row[i],"chap_id":chapter[i]["id"]} for i in range(len(row)) if row[i]!=None])
+        topic.extend([{"topic":row[i],"chap_id":chapter[i]["id"]} for i in range(len(row)) if row[i]!=None])
         data.append(row)
 
 
