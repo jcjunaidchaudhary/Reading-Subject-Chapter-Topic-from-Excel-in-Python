7c0418a8058939fca7cb20fe408dfc9e14e5960e
Juned <juned@pointpi.com>
2023-08-17 11:22:13 +0530

    Topic insertion handled

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import openpyxl
 
 #################################
-subject=[]
-chapter=[]
-topic=[]
+subjects=[]
+chapters=[]
+topics=[]
 #################################
 
 # Load the workbook
@@ -13,7 +13,7 @@ workbook = openpyxl.load_workbook("NEET-PLAN.xlsx")
 sheet_names = workbook.sheetnames
 
 for i in range(len(sheet_names)):
-    subject.append({'id':i+1,'name': sheet_names[i]})
+    subjects.append({'id':i+1,'name': sheet_names[i]})
 
 print(sheet_names)
 
@@ -21,14 +21,13 @@ print(sheet_names)
 # sheet = workbook.active
 
 
-def gen_num():
-    n=0
+def genNum():
+    n=1
     while True:
         yield n
         n+=1
 
-d=gen_num()
-d.__next__()
+gen_num=genNum()
 
 data=[]
 chapter_id=0
@@ -37,18 +36,18 @@ for sub_id in range(len(sheet_names)):
     sheet = workbook[sheet_names[sub_id]]
     for row in sheet.iter_rows(values_only=True):
         if count==0:
-            chapter.extend([{"id":d.__next__(),"chapter":row[i],"subject":sub_id+1} for i in range(len(row))])
+            chapter=[{"id":gen_num.__next__(),"chapter":row[i],"subject":sub_id+1} for i in range(len(row))]
             chapter_id+=len(row)
             count+=1
             continue
-        count+=1
-        topic.extend([{"topic":row[i],"chap_id":chapter[i]["id"]} for i in range(len(row)) if row[i]!=None])
+        chapters.extend(chapter)
+        topics.extend([{"topic":row[i],"chap_id":chapter[i]["id"]} for i in range(len(row)) if row[i]!=None])
         data.append(row)
 
 
-print(subject)
-print("\n\nchapter..................................",chapter)
-print("\n\ntopic......................................",topic)
+print(subjects)
+print("\n\nchapter..................................",chapters)
+print("\n\ntopic......................................",topics)
 
 
 # sheet = workbook[sheet_names[0]]
